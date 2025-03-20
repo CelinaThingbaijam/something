c07d8941d13429e331c47829785a4ef7b33ccfda
Celina Thingbaijam <celinathingbaijam@gmail.com>
2025-03-21 00:26:15 +0530

    in some2

--- hehe.py
+++ hehe.py
@@ -2,3 +2,11 @@ print("hello world")
 n = 5
 for i in range(5):
     print("something")
+    
+def maximum(n,n2):
+    if(n>n2):
+        print(n," is greater")
+    elif(n==n2):
+        print("both equal")
+    else:
+        print(n2," is greater")
